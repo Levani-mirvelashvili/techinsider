06893adec26786318b230f4643fd29422df90d7c
Levani Mirvelashvili <85246520+Levani-mirvelashvili@users.noreply.github.com>
2022-03-05 02:23:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function test() {
      alert('test has been run succesfully');
  }
+
 function o() {
   alert("Coming soon");
 }
 
 function myFunction() {
    var element = document.body;
-   element.classList.toggle("dark-mode");
+   element.classList.toggle("daark-modee");
 }
