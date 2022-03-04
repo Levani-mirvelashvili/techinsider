76e8aed5e02d1579f07b9da934c95a62814e02a1
Levani Mirvelashvili <85246520+Levani-mirvelashvili@users.noreply.github.com>
2022-03-04 23:40:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,23 @@ function o() {
   alert("Coming soon");
 }
 
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+const body = document.querySelector('body');
+const button = document.querySelector('#darkbutton');
+function toggleDark() {
+  if (body.classList.contains('dark')) {
+    body.classList.remove('dark');
+    localStorage.setItem("theme", "light");
+    button.innerHTML = "Dark mode";
+  } else {
+    body.classList.add('dark');
+    localStorage.setItem("theme", "dark");
+    button.innerHTML = "Dark mode";
+  }
 }
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add('dark');
+  button.innerHTML = "Dark mode";
+}
+
+document.querySelector('#darkbutton').addEventListener('click', toggleDark);
