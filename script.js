049776472e17ba5019c5cb9e74f7e482bab4a07d
Levani Mirvelashvili <85246520+Levani-mirvelashvili@users.noreply.github.com>
2022-03-05 00:21:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const body = document.querySelector('body');
-const button = document.querySelector('#darkbutton');
-function toggleDark() {
-  if (body.classList.contains('dark')) {
-    body.classList.remove('dark');
-    localStorage.setItem("theme", "light");
-    button.innerHTML = "Dark mode";
-  } else {
-    body.classList.add('dark');
-    localStorage.setItem("theme", "dark");
-    button.innerHTML = "Dark mode";
-  }
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
 }
-
-if (localStorage.getItem("theme") === "dark") {
-  body.classList.add('dark');
-  button.innerHTML = "Dark mode";
-}
-
-document.querySelector('#darkbutton').addEventListener('click', toggleDark);
