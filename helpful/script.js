cf4ecd3cb1ba95276fd7324d8c62d5aa7f9ca986
Levani Mirvelashvili <85246520+Levani-mirvelashvili@users.noreply.github.com>
2024-09-16 10:53:17 +0400

    Create script.js

---
+++ helpful/script.js
@@ -0,0 +1,12 @@
+function test() {
+     alert('test has been run succesfully');
+ }
+
+function o() {
+  alert("Coming soon");
+}
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("daark-modee");
+}
